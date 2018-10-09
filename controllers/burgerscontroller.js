4bba073c027c47e3c805ef40ce18689dceed56ea
Ahmed Gamal <ahmed.gamal.m@gmail.com>
2018-10-09 15:15:55 -0400

    fix

--- controllers/burgerscontroller.js
+++ controllers/burgerscontroller.js
@@ -1,29 +1,31 @@
 var express = require("express");
 
-var app = express.Router();
+var router = express.Router();
 var burger = require("../models/burger.js");
 
-app.get("/", function(req, res) {
+router.get("/", function(req, res) {
   res.redirect("/burgers");
 });
 
-app.get("/burgers", function(req, res) {
+router.get("/burgers", function(req, res) {
   burger.all(function(burgerData) {
     res.render("index", { burger_data: burgerData });
   });
 });
 
-app.post("/burgers/create", function(req, res) {
+
+router.post("/burgers/create", function(req, res) {
   burger.create(req.body.burger_name, function(result) {
     console.log(result);
     res.redirect("/");
   });
 });
 
-app.put("/burgers/:id", function(req, res) {
+
+router.put("/burgers/:id", function(req, res) {
   burger.update(req.params.id, function(result) {
     console.log(result);
   });
 });
 
-module.exports = app;
+module.exports = router;
