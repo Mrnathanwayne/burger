af1dcc0452b120bb617e9ab6d52d5f2833cc2839
Ahmed Gamal <ahmed.gamal.m@gmail.com>
2018-10-09 14:56:20 -0400

    fixing for deployment

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var exphbs = require("express-handlebars");
-var routes = require("./controllers/burgers_controller.js");
+var routes = require("./controllers/burgerscontroller.js");
 
 
 var PORT = process.env.PORT || 8080;
